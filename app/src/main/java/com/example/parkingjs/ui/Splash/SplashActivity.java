1d878da5200acaa1f659e098939858b12fed4b41
Oh-Donggeon <donggeon.dev@gmail.com>
2020-05-19 22:39:03 +0900

    splash animation added

--- app/src/main/java/com/example/parkingjs/ui/Splash/SplashActivity.java
+++ app/src/main/java/com/example/parkingjs/ui/Splash/SplashActivity.java
@@ -1,10 +1,16 @@
 package com.example.parkingjs.ui.Splash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import androidx.annotation.UiThread;
 
 import com.example.parkingjs.R;
 import com.example.parkingjs.databinding.ActivitySplashBinding;
 import com.example.parkingjs.ui.BaseActivity;
+import com.example.parkingjs.ui.main.MainActivity;
 
 public class SplashActivity extends BaseActivity<ActivitySplashBinding> {
 
@@ -12,6 +18,30 @@ public class SplashActivity extends BaseActivity<ActivitySplashBinding> {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        splashAnimation();
+    }
+
+    @UiThread
+    private void splashAnimation() {
+        Animation imageAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_splash);
+        binding.ivSplash.startAnimation(imageAnim);
+        imageAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 
     @Override
